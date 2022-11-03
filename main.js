1ba0911262e2d9bf28a6d274d100e34664c09ca6
Bamser <bamser@mail.ru>
2022-11-03 22:56:36 +0300

    mobile adaptive

--- main.js
+++ main.js
@@ -3,13 +3,28 @@ const swiper = new Swiper('.swiper', {
   slidesPerView: 3,
   slidesPerGroup: 3,
   speed: 1500,
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 3,
+    }
+  },
   
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
+    }
   
   });
+ 
