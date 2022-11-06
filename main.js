b13f200ac3afc187fda3fde7ea34b0be15687f90
Bamser <bamser@mail.ru>
2022-11-06 22:40:19 +0300

    adaptive slaider

--- main.js
+++ main.js
@@ -4,21 +4,20 @@ const swiper = new Swiper('.swiper', {
   slidesPerGroup: 3,
   speed: 1500,
   breakpoints: {
-    // when window width is >= 320px
     320: {
       slidesPerView: 1,
       slidesPerGroup: 1,
-
     },
     // when window width is >= 480px
     480: {
       slidesPerView: 2,
       slidesPerGroup: 2,
     },
-    // when window width is >= 640px
-    768: {
+    780: {
       slidesPerView: 3,
+      slidesPerGroup: 3,
     }
+   
   },
   
     navigation: {
