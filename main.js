8d98112907b9da64dc2c03930a0c55ab8fe37b16
Bamser <bamser@mail.ru>
2023-03-23 11:11:30 +0300

    add: light animation

--- main.js
+++ main.js
@@ -23,3 +23,27 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-left',
   },
 });
+
+// Анимашка при скролле
+function onEntry(entry) {
+  entry.forEach((change) => {
+    // Если элемент в зоне видимости, то класс добавляется
+    if (change.isIntersecting) {
+      change.target.classList.add('title__show');
+    }
+    // Если элемент пропал из зоны видимости, то класс убирается
+    else {
+      change.target.classList.remove('title__show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5],
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.title__animation');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
